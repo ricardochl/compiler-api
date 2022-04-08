c07b56eb460527787eac4658015753f13f2d41a0
Ricardo <jr.chavarria21@gmail.com>
2022-04-08 13:30:49 -0600

    Update cors

--- src/server.js
+++ src/server.js
@@ -32,24 +32,8 @@ const ads = [
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({origin:'*'}));
+app.use(cors({origin:'*', credentials: true}));
 app.use(morgan('combined'));
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
 
 app.get('/', (req, res) => {
     res.send(ads);
