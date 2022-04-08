8df5a849d012fa8727403108ff3c27cecbba9b4c
Ricardo <jr.chavarria21@gmail.com>
2022-04-08 13:30:03 -0600

    Update cors

--- src/server.js
+++ src/server.js
@@ -35,6 +35,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({origin:'*'}));
 app.use(morgan('combined'));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+
 app.get('/', (req, res) => {
     res.send(ads);
 });
